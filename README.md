887a9d1a172e2bdf6e9823a9ecacfbcedebd2c25
HoHuuThuan <thuanb2107182student.ctu.edu.vn>
2024-06-18 13:49:47 +0700

    Create component Header

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# LEARN-REACTJS
+# Run this to use npm
+npx create-react-app my-app
+
+# Install bosstrap and import to use bosstrap
+Open terminal in directory electronic-device run command
+    npm install bootstrap --save
+Use bosstrap in App.js
+    import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+# MÃ màu chính: #017EB6
+# Mã màu đen: #212529
---
+++ electronic-device/src/components/header/header.js
@@ -0,0 +1,40 @@
+import styles from "./header.css";
+import logo from "../../assets/logo-shop-electronic-device.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Header() {
+    return(
+      <div className="Header container-fluid">
+        <div className="background-header row align-items-center">
+          <div className="col-sm-1 d-none d-md-block">
+            <div className="logo"><a href="#"><img src={logo} alt="logo shop"></img></a></div>
+          </div>
+          <div className="col-sm-5">
+            <ul className="ul-nav d-flex mt-3 ">
+                <a href="#"><li>Trang chủ </li></a>
+                <a href="#"><li>Giới thiệu</li></a>
+                <a href="#"><li>Sản phẩm  </li></a>
+                <a href="#"><li>Tin tức   </li></a>
+                <a href="#"><li>Liên hệ   </li></a>
+            </ul>
+          </div>
+          <div className="search col-sm-4">  
+            <div className="input-group rounded">
+              <input type="search" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                <span className="input-group-text border-0" id="search-addon">
+                  <i style={{width: "35px"}} className="fas fa-search"></i>
+                </span>
+            </div>
+          </div>
+          <div className="col-sm-2 d-none d-sm-block">
+            <div className="card-and-user d-flex justify-content-center align-items-center">
+              <a href="#"><i className="fa-solid fa-cart-shopping"></i></a>
+              <a href="#"><i className="fa-solid fa-user"></i>         </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+export default Header;  
